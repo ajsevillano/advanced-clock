c0bddf2f88cdc08afda62ad9559982b2dd8c60ce
Sevi <correosevi@gmail.com>
2020-07-17 21:00:09 +0100

    Added a function to add leading zero on seconds and minutes

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 function getTime() {
-	let time, hours, minutes, seconds;
+	let time, hours, minutes, seconds, formattedSeconds;
 	time = new Date();
 	hours = time.getHours();
 	minutes = time.getMinutes();
 	seconds = time.getSeconds();
+
+	formattedSeconds = addLeadingZero(seconds);
+
+	// console.log(formattedSeconds[0]);
+	// console.log(formattedSeconds[1]);
 }
+
+function addLeadingZero(param) {
+	let formattedNumber = ('0' + param).slice(-2);
+	return formattedNumber;
+}
+
+getTime();
