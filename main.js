eb53ba199127aaf3cd29a66d8547331b2759ad7e
Sevi <correosevi@gmail.com>
2020-07-19 10:31:02 +0100

    Clock completed and working

--- main.js
+++ main.js
@@ -5,12 +5,20 @@ function getTime() {
 	minutes = time.getMinutes();
 	seconds = time.getSeconds();
 
+	formattedHours = addLeadingZero(hours);
+	formattedMinutes = addLeadingZero(minutes);
 	formattedSeconds = addLeadingZero(seconds);
 
+	//Querys
+	document.querySelector('.HoursFirstDigit').innerText = formattedHours[0];
+	document.querySelector('.HoursSecondDigit').innerText = formattedHours[1];
+	document.querySelector('.MinutesFirstDigit').innerText = formattedMinutes[0];
+	document.querySelector('.MinutesSecondDigit').innerText = formattedMinutes[1];
 	document.querySelector('.SecondsFirstDigit').innerText = formattedSeconds[0];
 	document.querySelector('.SecondsSecondDigit').innerText = formattedSeconds[1];
 
-	timer = setTimeout(() => {
+	//Timer
+	setTimeout(() => {
 		getTime();
 	}, 500);
 }
